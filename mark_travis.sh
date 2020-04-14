bfe4fd582c86b58f5654184984aee8210f7580bf
Tristan Glatard <tristan.glatard@concordia.ca>
2020-04-13 22:00:17 -0400

    Update following the fixing of https://github.com/travis-ci/travis.rb/issues/578

--- mark_travis.sh
+++ mark_travis.sh
@@ -12,10 +12,10 @@
 
 # A simple file containing 1 GH username per line
 github_accounts=$1
-total_tests=24 # don't parse the logs when status is 'green'
+total_tests=11 # don't parse the logs when status is 'green'
 
 # Change that with each assignment
-repo_prefix="tgteacher/bigdata-la1-w2019-"
+repo_prefix="tgteacher/la3-"
 
 echo -e "# GitHub\tRepo\tPassed\tFailed"
 for github in $(cat "${github_accounts}")
@@ -26,13 +26,19 @@ do
     then
         mark="${total_tests}\t0"
     else
-        temp_name="travis_mark.log"
-        # hacky due to https://github.com/travis-ci/travis.rb/issues/578
-        log_url=$(travis logs -r "${repo}" --pro --debug-http 2>/dev/null | grep ^Location: | cut -d \" -f 2)
-        rm ${temp_name} -f
-        wget -O ${temp_name} "${log_url}" 2>/dev/null
-        mark=$(grep -P "= [0-9]* failed, [0-9]* passed in [0-9]*\.[0-9]* seconds =" ${temp_name}\
-               | awk '{print $4"\t"$2}')
+        if [[ "${status}" = "failed" ]]
+        then
+            temp_name="travis_mark.log"
+            travis logs -r "${repo}" --pro > ${temp_name}
+            # When pytest isn't called with -q
+            mark=$(grep -P "= [0-9]* failed, [0-9]* passed in [0-9]*\.[0-9]* seconds =" ${temp_name}\
+                | awk '{print $4"\t"$2}' | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g")
+            # When pytest is called with -q
+            # mark=$(grep -P "[0-9]* failed, [0-9]* passed in [0-9]*\.[0-9]* seconds" ${temp_name}\
+            #    | awk '{print $3"\t"$1}' | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g")
+            else
+                mark="${status}"
+        fi
     fi
     echo -e "${github}\t${repo}\t${mark}"
 done
